807edaa8f8047912cd9dbbe3ecdae8bb1c745a0a
klimbus <klimbus2016@gmail.com>
2021-09-23 18:21:04 +0700

    Работа с вложенным массивом

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-productlist = ['milk', 'butter', 'potato', 'lemon', 'bread']
+productMinilist = ['apple', 'cheese', 'tomato', 'honey', 'carrot']
+productlist = ['milk', 'butter', 'potato', productMinilist, 'bread']
 print (productlist [2])
-print (productlist [4])
+print (productlist [3] [2])
