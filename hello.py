c984a6b6c896b21b12114a6c81cdfcfb34a83ad8
klimbus <klimbus2016@gmail.com>
2021-09-24 21:48:30 +0700

    Вводим целые + числа и высчитываем их по формуле геометрической прогрессии

--- hello.py
+++ hello.py
@@ -2,3 +2,4 @@ productMinilist = ['apple', 'cheese', 'tomato', 'honey', 'carrot']
 productlist = ['milk', 'butter', 'potato', productMinilist, 'bread']
 print (productlist [2])
 print (productlist [3] [2])
+
